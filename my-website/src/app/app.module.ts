98096fddda78d7a345a16dfe3467be943dc7dd89
Capucine Leclerc <capucine_49@hotmail.com>
2019-03-07 12:18:15 +0100

    material form + page + router

--- my-website/src/app/app.module.ts
+++ my-website/src/app/app.module.ts
@@ -6,34 +6,38 @@ import { HomeComponent } from './home/home.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { TypingAnimationDirective } from 'angular-typing-animation';
 import { RouterModule, Routes } from '@angular/router';
-import {MenuComponent} from './menu/menu.component';
-import {ContactComponent} from './contact/contact.component';
-import {ResumeComponent} from './resume/resume.component';
-import {AboutComponent} from './about/about.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContactComponent } from './contact/contact.component';
+import { ResumeComponent } from './resume/resume.component';
+import { AboutComponent } from './about/about.component';
 import { ServicesComponent } from './services/services.component';
-import { MatIconModule } from '@angular/material';
+import { MatIconModule, MatFormFieldModule, MatInputModule } from '@angular/material';
 
 const appRoutes: Routes = [
 
-  { path: '',
+  {
+    path: '',
     component: HomeComponent,
   },
-  { path: 'about',
+  {
+    path: 'about',
     component: AboutComponent,
     data: {
       title: 'about'
     }
   },
-  { path: 'resume',
-  component: ResumeComponent,
+  {
+    path: 'resume',
+    component: ResumeComponent,
     data: {
       title: 'resume'
     }
   },
-  { path: 'services',
-  component: ServicesComponent,
+  {
+    path: 'contact',
+    component: ContactComponent,
     data: {
-      title: 'services'
+      title: 'contact'
     }
   }
 ];
@@ -54,10 +58,14 @@ const appRoutes: Routes = [
     FlexLayoutModule,
     MatIconModule,
     RouterModule.forRoot(
-        appRoutes,
-        { enableTracing: false } // <-- debugging purposes only
-      ),
-      BrowserAnimationsModule
+      appRoutes,
+      { enableTracing: false } // <-- debugging purposes only
+    ),
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule
+
   ],
   exports: [RouterModule],
   providers: [],
